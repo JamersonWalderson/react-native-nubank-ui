97ce7a08c2cf6bee8540e1556cdf7b3868d2f2e3
jamerson walderson <jamersonwalderson@gmail.com>
2022-05-28 18:35:59 -0300

    feat(componente): criação do componente Divider

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./style";
+
+const Divider = () => {
+  return(
+    <Container>
+      <></>
+    </Container>
+  );
+}
+
+export default Divider;
---
+++ src/components/Divider/style.ts
@@ -0,0 +1,10 @@
+import { theme } from "../../global/styles";
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  margin-top: 6%;
+  border-bottom-width: 1;
+  border-bottom-color: black;
+  height: 5px;
+  width: 100%;
+`;
