67a01fc01f8e8bb065c7dc1debcc88e7ce2babe1
jamerson walderson <jamersonwalderson@gmail.com>
2022-05-29 00:53:59 -0300

    style(componente): aumento no tamanho e na cor da linha do componente Divider

--- src/components/Divider/style.ts
+++ src/components/Divider/style.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   margin-top: 6%;
-  border-bottom-width: 1px;
-  border-bottom-color: black;
+  border-bottom-width: 2px;
+  border-bottom-color: ${theme.colors.light};
   height: 5px;
   width: 100%;
 `;
